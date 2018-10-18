5ba55488c33223f0bfa2ae1efad211b28ce18a6d
Paulo Fonseca <paguso@cin.ufpe.br>
2018-10-18 17:07:13 -0300

    cst ukkonen ok

--- code/cst.py
+++ code/cst.py
@@ -12,6 +12,8 @@ class Node(object):
         self.r = r
         self.chd = {}
         self.slink = None
+    def __str__(self):
+        return "[id="+str(self.id)+ " l="+str(self.l)+" r="+str(self.r)+" slink="+(str(self.slink.id if self.slink else -1))+"]"
 
 def print_node(node, level, txt, end):
     print level*" ", 
@@ -47,41 +49,54 @@ def find_locus(root, x):
     return (cur, (0,0))
 
 
-
+''' 
+(u,(l,r)) ref canonica para vertice ativo da arvore T(i-1)
+'''
 def update((u,(l,r)), txt, i):
+    #print "start update form active <", u, "txt[%d:%d]"%(l,r), txt[l:r],">" 
     w_prev = None
     (is_term, w) = test_and_split((u,(l,r)), txt, i)
     while not is_term:
+        print "  add leaf to <", w, ">"
         leaf = Node(i)
+        w.chd[txt[i]] = leaf
         if w_prev :
             w_prev.slink = w
         w_prev = w
-        (u,(l,r)) = canonise((u,(l,r)), txt)
+        (u,(l,r)) = canonise((u.slink,(l,r)), txt)
         (is_term, w) = test_and_split((u,(l,r)), txt, i)
-    if w_prev :
+        print "w=",w
+    #print "  stopped at <", u, "txt[%d:%d]"%(l,r), txt[l:r],">" 
+    if w_prev and w:
         w_prev.slink = w
     return (u,(l,r))
 
 
+'''
+testa se no canonico (u,(l,r)) eh terminador i.e. se tem txt[i]-transicao
+e, caso negativo, torna-o explicito
+'''
 def test_and_split((u,(l,r)), txt, i):
+    print "testing <", u, "txt[%d:%d]"%(l,r), txt[l:r],"> has ",txt[i],"-trans" 
     if r<=l:
         if txt[i] in u.chd:
-            return (True, None)
+            return (True, u)
         else:
             return (False, u)
     else:
         v = u.chd[txt[l]]
-        if txt[u.l+(r-l)]==txt[i] :
+        if txt[v.l+(r-l)]==txt[i] :
             return (True, None)
         else:
-            w = Node(u.l+(r-l), u.r)
-            w.chd[txt[u.l+(r-l)]] = v
+            w = Node(v.l, v.l+(r-l))
             v.l += (r-l)
-            u.chd[txt[l]] = w
+            w.chd[txt[v.l]] = v
+            u.chd[txt[w.l]] = w
             return (False, w)
 
 
 def canonise((u,(l,r)), txt):
+    print "canonise <", u, "txt[%d:%d]"%(l,r), txt[l:r],">" 
     if r<=l:
         return (u,(l,r))
     else:
@@ -89,6 +104,7 @@ def canonise((u,(l,r)), txt):
         while (v.r-v.l)<=(r-l):
             u = v
             l += (v.r-v.l)
+            print "downto <", u, "txt[%d:%d]"%(l,r), txt[l:r],">" 
             if l<r:
                 v = u.chd[txt[l]]
         return (u,(l,r))
@@ -96,21 +112,24 @@ def canonise((u,(l,r)), txt):
 
 def ukkonen(txt, ab):
     n = len(txt)
-    ground = Node(-1)
-    root = Node(0,0)
+    ground = Node(-1,-1)
+    root = Node(-1,0)
+    root.slink = ground
     for a in ab:
         ground.chd[a] = root
     print_cst(ground, txt, 0) 
     (u,(l,r)) = (root,(0,0))
     for i in range(n):
+        print "start update form active <", u, "txt[%d:%d]"%(l,r), txt[l:r],">" 
         (u,(l,r)) = update((u,(l,r)), txt, i)
-        (u,(l,r)) = canonise((u,(l,r)), txt)
+        print "  stopped at <", u, "txt[%d:%d]"%(l,r), txt[l:r],">" 
+        (u,(l,r)) = canonise((u,(l,i+1)), txt)
         print_cst(ground, txt, i) 
 
 
 def main():
-    txt = "ababc"
-    ukkonen(txt, "abc")
+    txt = "senselessness"
+    ukkonen(txt, "elns")
 
 
 if __name__ == "__main__":
